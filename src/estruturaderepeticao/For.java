6f3f9455d5823c8ce9eb39c91a7398dfda21695b
k3vinrich4rd <kevinrichardve@gmail.com>
2022-06-30 19:32:33 -0300

    Introduzindo sources de estudo

---
+++ src/estruturaderepeticao/For.java
@@ -0,0 +1,9 @@
+package estruturaderepeticao;
+//Utilizando a estrutura de repetição for (Para) para contar de 1 a 3
+public class For {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Cambalhota");
+        }
+    }
+}
