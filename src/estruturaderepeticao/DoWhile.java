90009338b38b0cb52741f67767d9ef7c9e87c588
k3vinrich4rd <kevinrichardve@gmail.com>
2022-06-30 14:54:04 -0300

    Inserindo código de estudo

--- src/estruturaderepeticao/DoWhile.java
+++ src/estruturaderepeticao/DoWhile.java
@@ -1,2 +1,11 @@
-package estruturaderepeticao;public class DoWhile {
+package estruturaderepeticao;
+// Utilizando a estrutura de repetição do (faça) while (enquanto).
+public class DoWhile {
+    public static void main(String[] args) {
+        int contador = 0; // começo
+        do {
+            System.out.println("Cambalhota");
+            contador++; // contador do ciclo
+        }while (contador < 4); // fim
+    }
 }
