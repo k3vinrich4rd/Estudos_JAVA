759806dd16e7ad702db70f1fa23d7049411649a6
k3vinrich4rd <kevinrichardve@gmail.com>
2022-06-30 14:52:27 -0300

    Inserindo código de estudo

--- src/estruturaderepeticao/While2.java
+++ src/estruturaderepeticao/While2.java
@@ -1,2 +1,17 @@
-package estruturaderepeticao;public class While2 {
+package estruturaderepeticao;
+
+// Utilizando o while (Enquanto) para mudar o fluxo de um código utilizando o continue (continuar)
+public class While2 {
+    public static void main(String[] args) {
+        int contador = 0; // começo + 1
+        while (contador < 10) { // fim
+            contador++; // = contador = contador + 1
+            if (contador == 5 || contador == 7) { // Vai mostrar as cambalhotas, menos a 5 e 7
+                continue; // Vai mostrar as cambalhotas, menos a 5 e 7
+            }
+            System.out.println("Cambalhota " + contador);
+        }
+
+    }
 }
+
