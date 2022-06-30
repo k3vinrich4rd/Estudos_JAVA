c7c08aff0f4373d5a7ba6741509ccfc7e531c7a4
k3vinrich4rd <kevinrichardve@gmail.com>
2022-06-30 14:53:27 -0300

    Inserindo código de estudo

--- src/estruturaderepeticao/While3.java
+++ src/estruturaderepeticao/While3.java
@@ -1,2 +1,19 @@
-package estruturaderepeticao;public class While3 {
+package estruturaderepeticao;
+// Utilizando o while (Enquanto) para mudar o fluxo de um código utilizando o continue (continuar)
+// e break (Parar)
+
+public class While3 {
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador < 10) {
+            contador++;
+            if (contador == 2 || contador == 3 || contador == 4) {  //Vai mostrar as cambalhotas, menos a 2, 3 e 4
+                continue;
+            }
+            if (contador == 7) { // O código vai parar quando chegar na cambalhota 7, por conta do break
+                break; // (Parar)
+            }
+            System.out.println("Cambalhota" + contador);
+        }
+    }
 }
