df7a0c783dfb8594682fc0296a1c176268f9e68b
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-02 10:00:19 -0300

    Iserindo novos sources de exercício

---
+++ src/exerciciosestruturasderepeticao/ContadorBasico.java
@@ -0,0 +1,11 @@
+package exerciciosestruturasderepeticao;
+
+public class ContadorBasico {
+    public static void main(String[] args) {
+        int contador = 1;
+        while (contador <= 10) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
---
+++ src/exerciciosestruturasderepeticao/ContadorDeNumerosImparesFor.java
@@ -0,0 +1,14 @@
+package exerciciosestruturasderepeticao;
+
+public class ContadorDeNumerosImparesFor {
+    public static void main(String[] args) {
+        int numerosImpares = 0;
+        for (int numeros = 1; numeros <= 50; numeros++) {
+            if (numeros % 2 != 0) {
+                numerosImpares = numeros;
+                System.out.println(numerosImpares);
+            }
+
+        }
+    }
+}
