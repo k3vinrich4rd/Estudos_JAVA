07a643ac76a0eb084c3d238d55176e5cd328aec2
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-11 20:46:54 -0300

    Inserindo classe abstrata

---
+++ src3/abstracao/Fornecedor.java
@@ -0,0 +1,2 @@
+package abstracao;public class Fornecedor {
+}
---
+++ src3/abstracao/Pessoa.java
@@ -0,0 +1,34 @@
+package abstracao;
+
+public abstract class Pessoa { //Classe Mãe/ Pai
+    String nome; //Atributos
+    String endereco; //Atributos
+    String telefone; //Atributos
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+
+}
+
