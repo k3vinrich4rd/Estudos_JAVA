0b7667efb8525074877f3fdbd65d8cd8d74371d0
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-11 20:48:16 -0300

    Inserindo classe herdeira (filho)

--- src3/abstracao/Fornecedor.java
+++ src3/abstracao/Fornecedor.java
@@ -1,2 +1,39 @@
-package abstracao;public class Fornecedor {
+package abstracao;
+
+public class Fornecedor extends Pessoa {
+
+    private double valorCredito;
+    private double valorDivida;
+
+    private double saldo;
+
+    public Fornecedor() {
+        super();
+        this.valorCredito = valorCredito;
+        this.valorDivida = valorDivida;
+        this.saldo = 0;
+
+    }
+    public void obterSaldo(double valorCredito, double valorDivida){
+        saldo = valorCredito - valorDivida;
+        System.out.println("Meu saldo é de: " + saldo);
+    }
+
+    public double getValorCredito() {
+        return valorCredito;
+    }
+
+    public void setValorCredito(double valorCredito) {
+        this.valorCredito = valorCredito;
+    }
+
+    public double getValorDivida() {
+        return valorDivida;
+    }
+
+    public void setValorDivida(double valorDivida) {
+        this.valorDivida = valorDivida;
+    }
+
+
 }
