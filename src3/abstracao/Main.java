b56385ba72badccdbf5ba68a0251a10beac87127
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-11 20:49:56 -0300

    Inserindo Main para testes do projeto

--- src3/abstracao/Main.java
+++ src3/abstracao/Main.java
@@ -1,2 +1,11 @@
-package abstracao;public class Main {
+package abstracao;
+
+public class Main {
+    public static void main(String[] args) {
+        Fornecedor fornecedor = new Fornecedor();
+        fornecedor.obterSaldo(1500, 800);
+
+        Colaborador colaborador = new Colaborador();
+        colaborador.calcularSalario(15000, 2500);
+    }
 }
