0aa0dc3b9c71d9e5315201f57d71e09dda2436f5
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-17 16:54:54 -0300

    Inserindo atualizações feitas na Main do projeto

--- Poo - Treino  de conceitos (src)/canetaguanabara2/MainCaneta2.java
+++ Poo - Treino  de conceitos (src)/canetaguanabara2/MainCaneta2.java
@@ -1,2 +1,33 @@
-package canetaguanabara2;public class Caneta {
+package canetaguanabara2;
+
+public class MainCaneta2 {
+    public static void main(String[] args) {
+        Caneta2 caneta1 = new Caneta2();
+        caneta1.modelo = "Faber Castell";
+        caneta1.cor = "Preta";
+        caneta1.carga = 55;
+        caneta1.status();
+        caneta1.destampar();
+        caneta1.pintar();
+
+        Caneta2 caneta2 = new Caneta2();
+        caneta2.modelo = "Bic Cristal";
+        caneta2.cor = "Azul";
+        caneta2.carga = 87;
+        caneta2.status();
+        caneta2.destampar();
+        caneta2.escrever();
+
+        Caneta2 caneta3 = new Caneta2();
+        caneta3.modelo = "Bic Cristal";
+        caneta3.cor = "Vermelha";
+        caneta3.carga = 73;
+        caneta3.status();
+        caneta3.destampar();
+        caneta3.rabiscar();
+
+
+
+
+    }
 }
