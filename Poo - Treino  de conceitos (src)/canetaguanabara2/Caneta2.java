0e7312caf530570019bd39096bab0985ebcb39e0
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-17 17:34:59 -0300

    Inserindo novas atualizações

--- Poo - Treino  de conceitos (src)/canetaguanabara2/Caneta2.java
+++ Poo - Treino  de conceitos (src)/canetaguanabara2/Caneta2.java
@@ -7,6 +7,14 @@ public class Caneta2 {
     protected int carga;
     private boolean tampada;
 
+    public Caneta2(String modelo, String cor, double ponta, int carga) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ponta = ponta;
+        this.carga = carga;
+
+    }
+
     public void status() {
         System.out.println("----- Informações sobre a caneta -----");
         System.out.println("Modelo: " + this.modelo);
@@ -26,7 +34,7 @@ public class Caneta2 {
 
     public void escrever() {
         if (this.tampada) {
-            System.out.println("Impossivel escrever, a caneta está tampada");
+            System.out.println("Impossivel escrever, a caneta está tampada\n");
         } else {
             System.out.println("A caneta está escrevendo\n");
         }
@@ -34,7 +42,7 @@ public class Caneta2 {
 
     public void rabiscar() {
         if (this.tampada) {
-            System.out.println("Impossivel efetuar está ação, destampe a caneta");
+            System.out.println("Impossivel efetuar está ação, destampe a caneta\n");
         } else {
             System.out.println("A caneta está rabiscando\n");
         }
@@ -42,12 +50,54 @@ public class Caneta2 {
 
     public void pintar() {
         if (this.tampada) {
-            System.out.println("Impossivel efetuar a pintura, destampe a caneta ");
+            System.out.println("Impossivel efetuar a pintura, destampe a caneta\n");
         } else {
             System.out.println("A caneta está pitando\n");
         }
 
     }
+
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(double ponta) {
+        this.ponta = ponta;
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+
 
 
 }
--- Poo - Treino  de conceitos (src)/canetaguanabara2/MainCaneta2.java
+++ Poo - Treino  de conceitos (src)/canetaguanabara2/MainCaneta2.java
@@ -2,31 +2,20 @@ package canetaguanabara2;
 
 public class MainCaneta2 {
     public static void main(String[] args) {
-        Caneta2 caneta1 = new Caneta2();
-        caneta1.modelo = "Faber Castell";
-        caneta1.cor = "Preta";
-        caneta1.carga = 55;
+        Caneta2 caneta1 = new Caneta2("Faber Castell", "Preta", 0.6, 76);
         caneta1.status();
-        caneta1.destampar();
+        caneta1.tampar();
         caneta1.pintar();
 
-        Caneta2 caneta2 = new Caneta2();
-        caneta2.modelo = "Bic Cristal";
-        caneta2.cor = "Azul";
-        caneta2.carga = 87;
+        Caneta2 caneta2 = new Caneta2("Bic Cristal", "Azul", 0.5, 87);
         caneta2.status();
         caneta2.destampar();
         caneta2.escrever();
 
-        Caneta2 caneta3 = new Caneta2();
-        caneta3.modelo = "Bic Cristal";
-        caneta3.cor = "Vermelha";
-        caneta3.carga = 73;
+        Caneta2 caneta3 = new Caneta2("Bic Cristal", "Vermelha", 1.0, 73);
         caneta3.status();
         caneta3.destampar();
         caneta3.rabiscar();
-
-
 
 
     }
