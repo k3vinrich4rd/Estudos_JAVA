7c5761fb86fc9a7dac93ae44cb134fb8105c31b4
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-18 02:14:57 -0300

    Inserindo novas atualizações

--- Poo - Treino  de conceitos (src)/uec/Luta.java
+++ Poo - Treino  de conceitos (src)/uec/Luta.java
@@ -20,5 +20,37 @@ public class Luta {
     public void lutar(){
 
     }
+
+    public String getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(String desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public String getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(String desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
 }
 
