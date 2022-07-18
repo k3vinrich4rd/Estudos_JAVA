b4ccc93427a5f342d7cd7de3cdadd95f66cfb94f
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-17 23:11:50 -0300

    Inserindo atualizações nos sources do código

--- Poo - conceitos (src)/interfacee/ControleRemoto.java
+++ Poo - conceitos (src)/interfacee/ControleRemoto.java
@@ -1,96 +1,142 @@
 package interfacee;
 
-public class ControleRemoto implements Controlador{
+public class ControleRemoto implements Controlador {
     //Atributos
     private int volume;
     private boolean ligado;
     private boolean tocando;
 
-    public ControleRemoto(int volume, boolean ligado, boolean tocando) {
+    // Método construtor
+    public ControleRemoto() {
         this.volume = 50;
         this.ligado = false;
         this.tocando = false;
 
-
     }
 
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public boolean getLigado() {
-        return ligado;
-    }
-
-    public void setLigado(boolean ligado) {
-        this.ligado = ligado;
-    }
-
-    public boolean getTocando() {
-        return tocando;
-    }
-
-    public void setTocando(boolean tocando) {
-        this.tocando = tocando;
-    }
-
+    //Métodos Abstratos:
     @Override
     public void ligar() {
-        this.setLigado(false);
+        this.setLigado(true);
 
     }
 
+    //Métodos Abstratos:
     @Override
     public void desligar() {
-        this.setLigado(true);
+        this.setLigado(false);
     }
 
+    //Métodos Abstratos:
     @Override
     public void abrirMenu() {
-        System.out.println("Está ligado? " + this.getLigado());
-        System.out.println("Está tocando? " + this.getTocando());
-        System.out.println("Volume: " + this.getVolume());
-        for (int contador = 0; contador < getVolume(); contador+=10) {
-            System.out.println("[]");
+        if (getLigado()) {
+            System.out.println("Você está no menu do seu controle: ");
+            System.out.println("Está ligado? " + this.getLigado());
+            System.out.println("Está tocando? " + this.getTocando());
+            System.out.println("Volume: " + this.getVolume());
+
+            for (int contador = 0; contador <= this.getVolume(); contador += 10) {
+                System.out.print("[]");
+            }
+        }else {
+            System.out.println("Impossivel efetuar essa ação a Tv está desligada");
+        }
+
+    }
+
+
+    //Métodos Abstratos:
+    @Override
+    public void fecharMenu() {
+        System.out.println("Ok, fechando menu...");
+    }
+
+    //Métodos Abstratos:
+    @Override
+    public void maisVolume() {
+        if (this.getLigado()) {
+            this.setVolume(this.getVolume() + 5);
+        } else {
+            System.out.println("Impossivel aumentar volume");
+        }
+
+    }
+
+    //Métodos Abstratos:
+    @Override
+    public void menosVolume() {
+        if (this.getLigado()) {
+            this.setVolume(this.getVolume() - 5);
+
+        } else {
+            System.out.println("Impossivel diminuir volume");
         }
     }
 
-    @Override
-    public void fecharMenu() {
-        System.out.println("Fechando menu...");
-    }
-
-    @Override
-    public void maisVolume() {
-
-    }
-
-    @Override
-    public void menosVolume() {
-
-    }
-
+    //Métodos Abstratos:
     @Override
     public void ligarMudo() {
-
+        if (this.getLigado() && getVolume() > 0) {
+            this.setVolume(0);
+        }else {
+            System.out.println("A sua televisão já está sem volume");
+        }
     }
 
+    //Métodos Abstratos:
     @Override
     public void deligarMudo() {
-
+        if (this.getLigado() && this.getVolume() == 0) {
+            this.setVolume(50);
+        }else {
+            System.out.println("O modo mudo da sua televisão já está desabilitado");
+        }
     }
 
+    //Métodos Abstratos:
     @Override
     public void play() {
-
+        if (this.getLigado() && !(this.getTocando())) {
+            this.setTocando(true);
+        } else {
+            System.out.println("Não consegui reproduzir ");
+        }
     }
 
+    //Métodos Abstratos:
     @Override
     public void pause() {
+        if (this.getLigado() && this.getTocando()) {
+            this.setTocando(false);
 
+        } else {
+            System.out.println("Não consegui pausar");
+        }
     }
+
+    private int getVolume() {
+        return volume;
+    }
+
+    private void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private boolean getLigado() {
+        return ligado;
+    }
+
+    private void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    private boolean getTocando() {
+        return tocando;
+    }
+
+    private void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+
 }
---
+++ Poo - conceitos (src)/interfacee/MainControleRemoto.java
@@ -0,0 +1,11 @@
+package interfacee;
+
+public class MainControleRemoto {
+    public static void main(String[] args) {
+        ControleRemoto controleRemoto = new ControleRemoto();
+        controleRemoto.abrirMenu();
+        controleRemoto.deligarMudo();
+        controleRemoto.fecharMenu();
+
+    }
+}
