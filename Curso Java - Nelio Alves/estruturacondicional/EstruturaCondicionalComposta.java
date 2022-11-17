20a84f26e493aab6ccfdd4d2ca0d8883c6a18891
k3vinrich4rd <kevinrichardve@gmail.com>
2022-11-17 14:55:00 -0300

    Primeirar alteração

--- Curso Java - Nelio Alves/estruturacondicional/EstruturaCondicionalComposta.java
+++ Curso Java - Nelio Alves/estruturacondicional/EstruturaCondicionalComposta.java
@@ -3,13 +3,13 @@ package estruturacondicional;
 import java.util.Scanner;
 
 public class EstruturaCondicionalComposta {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int hora = input.nextInt();
 
-        if (hora < 12){
+        if (hora < 12) {
             System.out.println("Bom dia");
-        }else {
+        } else {
             System.out.println("Boa tarde");
         }
     }
