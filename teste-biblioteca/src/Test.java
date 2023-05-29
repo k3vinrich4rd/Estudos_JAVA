5bbd85217a5fe6cf019dc7d097541dd55017430d
k3vinrich4rd <kevinrichardve@gmail.com>
2023-05-29 08:09:07 -0300

    Inserindo estrutura do projeto

--- teste-biblioteca/src/Test.java
+++ teste-biblioteca/src/Test.java
@@ -1,10 +1,9 @@
 import br.com.alura.bytebank.template.CurrentAccount;
-import br.com.estudos.alura.taxacalculator.test.TestTaxable;
 
 public class Test {
     public static void main(String[] args) {
         CurrentAccount currentAccount = new CurrentAccount();
-        TestTaxable testTaxable;
-        System.out.println(testTaxable);
+        currentAccount.depositInAccount(1000);
+        System.out.println(currentAccount.getBalance());
     }
 }
