0b13b4c925d3635298b6e74662f7fb7eaac8f84a
k3vinrich4rd <kevinrichardve@gmail.com>
2024-03-25 12:37:15 -0300

    Inserindo mudanca logica na classe de teste

--- Java Orientacao e Objetos - Caelum/poo/exercises/Conta.java
+++ Java Orientacao e Objetos - Caelum/poo/exercises/Conta.java
@@ -9,25 +9,26 @@ public class Conta {
     Data dataDeAbertura;
 
     void sacar(double quantia) {
-        this.saldo = -quantia;
+        this.saldo = this.saldo - quantia;
     }
 
     void depositar(double quantia) {
-        this.saldo = +quantia;
+        this.saldo = this.saldo + quantia;
     }
 
-    void calcularRendimento() {
-        double rendimento = 0.1;
-        this.saldo = this.saldo * rendimento;
+    double calcularRendimento() {
+        return this.saldo * 0.1;
     }
 
-    String recuperarDadosParaImpressao(){
+    String recuperarDadosParaImpressao() {
         return
                 "Informações da conta: \n" +
                         "Nome do proprietário da conta: " + this.titular + "\n" +
                         "Numero da conta: " + this.numero + "\n" +
                         "Agência da conta: " + this.agencia + "\n" +
-                        "Data de abertura da conta: " + "\nDia: " + this.dataDeAbertura.dia +"\n"+
-                        "Mes: " + this.dataDeAbertura.mes + "\n" + "Ano: " + this.dataDeAbertura.ano ;
+                        "Saldo da conta: " + this.saldo + "\n" +
+                        "Rendimento da conta: " + this.calcularRendimento() + "\n" +
+                        "Data de abertura da conta: " + "\nDia: " + this.dataDeAbertura.dia + "\n" +
+                        "Mes: " + this.dataDeAbertura.mes + "\n" + "Ano: " + this.dataDeAbertura.ano;
     }
 }
--- Java Orientacao e Objetos - Caelum/poo/exercises/TestaConta.java
+++ Java Orientacao e Objetos - Caelum/poo/exercises/TestaConta.java
@@ -3,16 +3,24 @@ package poo.exercises;
 public class TestaConta {
     public static void main(String[] args) {
 
+        Data data = new Data();
         Conta conta = new Conta();
 
         conta.titular = "Kevin Richard";
         conta.numero = 12345678;
         conta.agencia = "agência teste";
         conta.saldo = 1000.0;
-        conta.dataDeAbertura.dia = 20;
-        conta.dataDeAbertura.mes = 3;
-        conta.dataDeAbertura.ano = 2024;
+        conta.sacar(100.0);
+        conta.depositar(200.0);
 
+
+        data.dia = 20;
+        data.mes = 3;
+        data.ano = 2024;
+
+        conta.dataDeAbertura = data;
         System.out.println(conta.recuperarDadosParaImpressao());
+
+
     }
 }
